50ef18229c56465a3703efeda3daa7300b088d33
Tim Velten <tim.velten@gmail.com>
2019-10-31 20:17:02 -0400

    now 77

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload Part 73
+          Edit <code>src/App.js</code> and save to reload Part 77
         </p>
         <a
           className="App-link"
